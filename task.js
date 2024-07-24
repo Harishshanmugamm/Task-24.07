777b78d072f4d0027ebb7b0901d53ad5d65cc4d9
Harishshanmugamm <harishshanmugamm@outlook.com>
2024-07-24 16:22:01 +0530

    Final Commit -2

--- task.js
+++ task.js
@@ -1,5 +1,6 @@
 let A = [1, 2, 2, 3, 4, 4, ];
 let B = [6,4,5];
+k=5
 let merged = [...A, ...B];
 console.log('Merged array', merged);// 1. Merging two arrays
 merged.sort()  // 2.Array Sorting - sort()
@@ -8,13 +9,8 @@ merged.reverse()   // 2.Array Sorting - reverse()
 console.log('Merged array reversed', merged);
 let deduplicated=[...new Set(A)];
 console.log('Deduplication of Array',deduplicatedArr)  //3. Deplicated Array
-
-function rotating_An_Array(A, k) {
-    k = k % A.length;
-    return A.slice(-k).concat(A.slice(0, -k));
-}
-console.log('Rotating an Array', rotating_An_Array(A, 2)); //4. Rotating an array
-
+k = k % A.length;
+console.log('Rotating an Array', A.slice(-k).concat(A.slice(0, -k))); //4. Rotating an array
 function isequals(arr1, arr2) {
     if (arr1.length !== arr2.length) return false;
     for (let i = 0; i < arr1.length; i++) {
@@ -22,6 +18,5 @@ function isequals(arr1, arr2) {
     }
     return true;
 }
-
 console.log(isequals(A, B));  // 5. Check Equals from A B 
 
